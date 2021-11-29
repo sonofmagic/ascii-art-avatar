8da44aa1d8c1a89d22e5867438ba179833dd984c
SonOfMagic <qq1324318532@gmail.com>
2021-11-29 23:27:20 +0800

    feat: vercel LD_LIBRARY_PATH

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
+// For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`
+}
+
 const Koa = require('koa')
 const cors = require('@koa/cors')
 const path = require('path')
