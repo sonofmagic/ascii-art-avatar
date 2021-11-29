6dcf627909dccdef6a91eb5a53a69e70ab612777
SonOfMagic <qq1324318532@gmail.com>
2021-11-30 00:08:42 +0800

    fixed: try set LD_PRELOAD

--- src/app.js
+++ src/app.js
@@ -9,7 +9,11 @@ if (
     process.env.PWD
   }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`
 }
+
+process.env.LD_PRELOAD = '/var/task/node_modules/canvas/build/Release/libz.so.1'
+
 // https://github.com/vercel/vercel/issues/3460#issuecomment-583072848
+
 const Koa = require('koa')
 const cors = require('@koa/cors')
 const path = require('path')
